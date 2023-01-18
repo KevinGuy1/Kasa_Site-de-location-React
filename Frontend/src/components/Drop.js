dd073a9cb5113ae429b480e1df0104910f4d9c45
Hygei <k.guy.35@gmail.com>
2023-01-18 18:45:11 +0100

    animation ouverture et fermeture des sujets callapses

--- Frontend/src/components/Drop.js
+++ Frontend/src/components/Drop.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Vector from '../images/vector.png'
 
 function Drop({ title, content }) {
+    const [isOpen, setIsOpen] = useState(false);
 
+
+    function handleClick() {
+        setIsOpen(!isOpen)
+    }
 
 
 
     return (
-        <div className='drop'>
-            <p>{title}</p>
-            <p>{content}</p>
-        </div>
+        <>
+            <div className='drop' onClick={handleClick}>
+                <h2>{title}</h2>
+                <img className={`arrow ${isOpen && "isActive"}`} src={Vector} alt="Fleche ouvert ou fermé" />
+            </div>
+            <div>
+                {isOpen ? <p>{content}</p> : null}
+            </div>
+        </>
     );
 }
 
