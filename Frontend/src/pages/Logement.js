d3451deaaa79f141a5dccd1ffebd0bf16b02c620
Hygei <k.guy.35@gmail.com>
2023-01-25 20:21:35 +0100

    mise en place des éléments de la page logements

--- Frontend/src/pages/Logement.js
+++ Frontend/src/pages/Logement.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Data from "../data.json"
 import Carrousel from '../components/Carrousel';
+import Drop from "../components/Drop"
 
 function Logement() {
     let location = useLocation()
@@ -12,6 +13,34 @@ function Logement() {
     return (
         <div className='logement'>
             <Carrousel slides={logement.pictures} />
+            <div>
+                <div className='logement__titles'>
+                    <h1>{logement.title}</h1>
+                    <p>{logement.location}</p>
+                    {logement.tags.map((tags) => {
+                        return (
+                            <div>
+                                {tags}
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className='logement__user'>
+                    <div className='host'>
+                        <p>{logement.host.name}</p>
+                        <img src={logement.host.picture} alt="host" />
+                    </div>
+                    <div className='stars'>
+
+                    </div>
+                </div>
+            </div>
+            <Drop
+                title="Description"
+                content={logement.description} />
+            <Drop
+                title="Equipments"
+                content={logement.equipments} />
         </div>
     );
 }
