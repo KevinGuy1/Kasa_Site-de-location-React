9fbb8b227713df1cb9470f6f50c640e9132ae141
Hygei <k.guy.35@gmail.com>
2023-01-26 13:52:03 +0100

    css de la page logement / Titres et profil de l'hote + drop

--- Frontend/src/pages/Logement.js
+++ Frontend/src/pages/Logement.js
@@ -13,34 +13,40 @@ function Logement() {
     return (
         <div className='logement'>
             <Carrousel slides={logement.pictures} />
-            <div>
-                <div className='logement__titles'>
+            <div className='logement__head'>
+                <div className='logement__head__titles'>
                     <h1>{logement.title}</h1>
                     <p>{logement.location}</p>
-                    {logement.tags.map((tags) => {
-                        return (
-                            <div>
-                                {tags}
-                            </div>
-                        );
-                    })}
+                    <div className='tags'>
+                        {logement.tags.map((tags) => {
+                            return (
+                                <div className='tag'>
+                                    {tags}
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
-                <div className='logement__user'>
+                <div className='logement__head__user'>
                     <div className='host'>
-                        <p>{logement.host.name}</p>
+                        <div className='name'>{logement.host.name}</div>
                         <img src={logement.host.picture} alt="host" />
                     </div>
                     <div className='stars'>
-
+                        étoiles
                     </div>
                 </div>
             </div>
-            <Drop
-                title="Description"
-                content={logement.description} />
-            <Drop
-                title="Equipments"
-                content={logement.equipments} />
+            <div className='drops'>
+                <div className='droplog'><Drop title="Description" content={logement.description} /></div>
+                <div className='droplog'><Drop title="Equipments" content={logement.equipments.map((equipments) => {
+                    return (
+                        <div className='equipment'>
+                            {equipments}
+                        </div>
+                    );
+                })} /></div>
+            </div>
         </div>
     );
 }
